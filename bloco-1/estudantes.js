79078634d963cf0707af0345611ce469ed34d357
simonesafira2007 <simone.safira2007@gmail.com>
2020-09-18 10:40:23 -0300

    adicionando itens ao arquivo

--- bloco-1/estudantes.js
+++ bloco-1/estudantes.js
@@ -3,7 +3,7 @@ const estudantes = [
     "id": 1,
     "nome": "Célia",
     "sobrenome":  "Aquino",
-    "idade": 55,
+    "idade": 34,
     "RG": 2223334,
     "cpf": 111222333444,
     "estadoCivil": "casada",
@@ -18,6 +18,193 @@ const estudantes = [
     "status":"em andamento",
     "professora": true
     
+    },
+    {
+
+    "id": 2,
+    "nome": "Lucas",
+    "sobrenome":  "Santos",
+    "idade": 23,
+    "RG": 1234567,
+    "cpf": 22233344455,
+    "estadoCivil": "solteiro",
+    "filhos": "Rachel",
+    "moradia": {"rua": "Rua das Moças",
+                    "numero": 19, 
+                    "bairro": "Arruda",
+                    "cidade": "Recife" }, 
+    "universidade": "UFRPE",
+    "curso": "Letras",
+    "matricula": 98765 ,
+    "status":"concluído",
+    "professora": false
+            
+    },
+    {
+        "id": 3,
+        "nome": "Maria",
+        "sobrenome":  "Fernanda",
+        "idade": 25,
+        "RG": 3567801,
+        "cpf": 98076544321,
+        "estadoCivil": "casada",
+        "filhos": "Solange",
+        "moradia": {"rua": "Rua Padre Lemos",
+                    "numero": 1567, 
+                    "bairro": "Boa Viagem",
+                    "cidade": "Recife" }, 
+        "universidade": "UNICAP",
+        "curso": "Geografia",
+        "matricula": 45328 ,
+        "status":"em andamento",
+        "professora": true
+           
+    },
+    {
+        "id": 4,
+        "nome": "Sônia",
+        "sobrenome":  "Lima",
+        "idade": 43,
+        "RG": 4509724,
+        "cpf": 44466677740,
+        "estadoCivil": "casada",
+        "filhos": ["Vítor" , "Marina"],
+        "moradia": {"rua": "Rua do cravo",
+                    "numero": 45, 
+                    "bairro": "Madalena",
+                    "cidade": "Recife" }, 
+        "universidade": "UFPE",
+        "curso": "História",
+        "matricula": 85632 ,
+        "status":"concluído",
+        "professora": true
+            
+    },
+    {
+
+        "id": 5,
+        "nome": "Kátia",
+        "sobrenome":  "Bezerra",
+        "idade": 21,
+        "RG": 2345678,
+        "cpf": 74902460981,
+        "estadoCivil": "solteira",
+        "filhos": "Leonardo",
+        "moradia": {"rua": "Rua do Sol",
+                    "numero": 7602, 
+                    "bairro": "Casa Amarela",
+                    "cidade": "Recife" }, 
+        "universidade": "UPE",
+        "curso": "Medicina",
+        "matricula": 49056 ,
+        "status":"concluído",
+        "professora": false
+               
+    },
+    {
+
+        "id": 6,
+        "nome": "Patrícia",
+        "sobrenome":  "Souza",
+        "idade": 30,
+        "RG": 6557884,
+        "cpf": 100034567346,
+        "estadoCivil": "casada",
+        "filhos": "",
+        "moradia": {"rua": "Rua Nova",
+                    "numero": 568, 
+                    "bairro": "Boa Vista",
+                    "cidade": "Recife" }, 
+        "universidade": "UFPE",
+        "curso": "Matemática",
+        "matricula": 77564 ,
+        "status":"em andamento",
+        "professora": true
+                
+    },
+    {
+
+        "id": 7,
+        "nome": "Marcos",
+        "sobrenome":  "Silva",
+        "idade": 41,
+        "RG": 4659084,
+        "cpf": 51789345688,
+        "estadoCivil": "casado",
+        "filhos": ["Heitor", "Poliana"],
+        "moradia": {"rua": "Rua da Imperatriz",
+                    "numero": 76, 
+                    "bairro": "Santo Antonio",
+                    "cidade": "Recife" }, 
+        "universidade": "UFPE",
+        "curso": "Ciência da Computação",
+        "matricula": 86023 ,
+        "status":"concluído",
+        "professora": true
+                   
+    },
+    {
+
+        "id": 8,
+        "nome": "Jucinara",
+        "sobrenome":  "Abranches",
+        "idade": 32,
+        "RG": 6439032,
+        "cpf": 20648713457,
+        "estadoCivil": "divorciada",
+        "filhos": "Ivan",
+        "moradia": {"rua": "Rua da Abolição",
+                    "numero": 509, 
+                    "bairro": "Cordeiro",
+                    "cidade": "Recife" }, 
+        "universidade": "UFPE",
+        "curso": "Ciências Biológicas",
+        "matricula": 98761 ,
+        "status":"concluído",
+        "professora": true
+                   
+    },
+    {
+
+        "id": 9,
+        "nome": "Marcelo",
+        "sobrenome":  "Soares",
+        "idade": 38,
+        "RG": 7482943,
+        "cpf": 93025680301,
+        "estadoCivil": "solteiro",
+        "filhos": "Sabrina",
+        "moradia": {"rua": "Rua Sete de Setembro",
+                    "numero": 34, 
+                    "bairro": "Parnamirim",
+                    "cidade": "Recife" }, 
+        "universidade": "UFRPE",
+        "curso": "Educação Física",
+        "matricula": 56372 ,
+        "status":"concluído",
+        "professora": true
+                       
+    },
+    {
+
+        "id": 10,
+        "nome": "Fabiana",
+        "sobrenome":  "Macêdo",
+        "idade": 19,
+        "RG": 5208712,
+        "cpf": 52648293958,
+        "estadoCivil": "solteira",
+        "filhos": "",
+        "moradia": {"rua": "Rua do Farol",
+                    "numero": 732, 
+                    "bairro": "Engenho do meio",
+                    "cidade": "Recife" }, 
+        "universidade": "Unicap",
+        "curso": "Pedagogia",
+        "matricula": 56372 ,
+        "status":"em andamento",
+        "professora": false
+                   
     }
     
     ]
--- bloco-1/receitas.js
+++ bloco-1/receitas.js
@@ -5,20 +5,55 @@ const receitas = [{
     "ingredientes": ["macarrão espaguete", "azeite", "alho", "sal", "tomate", "queijo ralado", "cebola", "cenoura", "abobrinha", "berinjela", "pimenta-do-reino"],
     "rendimentoPorPorcao": 3,
     "categoria": "Massa",
-    "origem": "grega",
     "dificuldade": "fácil",
     "tempoDePreparo": "30 minutos",
     "ocasioes":["dia-a-dia", "ocasiões especiais"],
-    "criador": {"chef": "Mariana Fonseca",
-                "restaurante": "Pizzaria Atlântico",
-                "casada": true ,
-                "filhos": false,
-                "cidade": "Recife"
+    "criador": "Mariana Fonseca",
+               
                 
-               },   
+   }, 
+   {  
+   "id": 2,
+   "nome": "Salada Grega",
+   "metodoDePreparo": "cru",
+   "ingredientes": ["pimentão vermelho", "pimentão amarelo", "sal", "azeite", "cebola roxa", "orégano", "tomate", "queijo feta", "azeitona preta", "tomates",  "pepino japonês"],
+   "rendimentoPorPorcao": 3,
+   "categoria": "Massa",
+   "dificuldade": "fácil",
+   "tempoDePreparo": "10 minutos",
+   "ocasioes":["dia-a-dia", "ocasiões especiais"],
+   "criador": "Lourdes Borba",
+                          
+   },
+   {
+   "id": 3,
+   "nome": "Cachupa",
+   "metodoDePreparo": "Refogado",
+   "ingredientes": [" carne bovina", "Pé de porco", "bacon", "sal", "chouriço", "milho", "orégano", "feijão", "grão-de-bico", "cebola", "louro",  "batata-doce ","couve ", "mandioca", "sal", "azeite", "pimenta-do-reino"],
+   "rendimentoPorPorcao": 6,
+   "categoria": "Carne",
+   "dificuldade": "Médio",
+   "tempoDePreparo": "30 minutos",
+   "ocasioes":"Festas",
+   "criador": "Aline Chermoula",
+                             
 
+   },
+   {
+      "id": 4,
+      "nome": "Rabanada low carb",
+      "metodoDePreparo": "Fritura",
+      "ingredientes": [" farinha", "farinha de amêndoa", "fermento químico", "sal", "psyllium", "extrato de baunilha", " canela em pó", "vinagre de maçã", "óleo ou óleo de coco "],
+      "rendimentoPorPorcao": 6,
+      "categoria": "Doce Caseiro",
+      "dificuldade": "Médio",
+      "tempoDePreparo": "30 minutos",
+      "ocasioes": ["Festas","Natal"],
+      "criador": "Saschi ",
+
+   }            
   
      
-  }]
+   ]
   
   console.log(receitas)
