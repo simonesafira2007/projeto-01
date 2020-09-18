a219653f368f0412ed0d21ff520813fd28c973b3
simonesafira2007 <simone.safira2007@gmail.com>
2020-09-17 22:30:48 -0300

    complemento arquivo filmes

--- bloco-1/filmes.js
+++ bloco-1/filmes.js
@@ -5,9 +5,7 @@ const filmes = [{
     "tipoDoFilme": "longa-metragem",
     "diretor": ["Victor Fleming", "George Cukor", "Sam Wood"],
     "categoria": ["Drama","Romance"],
-    "anoDeProducao": 1939,
     "anoLancamento": 1940,
-    "duracao": "3h 58min",
     "casting": {
         "atorPrincipal": "Clark Gable",
         "atrizPrincipal": "Vivien Leigh",
@@ -16,7 +14,165 @@ const filmes = [{
     "cor": "colorido",
     "nacionalidade": "EUA"
     
-    }]
+    },
+{
+
+    "id": 2,
+    "titulo": "Capitão América: Soldado Invernal",
+    "distribuidor": "Buena Vista(Disney)",
+    "tipoDoFilme": "longa-metragem",
+    "diretor": ["Joe Russo" ,"Anthony Russo"],
+    "categoria": ["Aventura","Ação"],
+    "anoLancamento": 2014,
+    "casting": {
+        "atorPrincipal": "Robert Redford",
+        "atrizPrincipal": "Scarlett Johansson",
+        "atorCoadjuvante":"Samuel L.Jackson"
+    },
+    "cor": "colorido",
+    "nacionalidade": "EUA"
+    
+},
+{
+    "id": 3,
+    "titulo": "Amor em Sampa",
+    "distribuidor": ["Pulsar Cinema", "Coração da Selva"],
+    "tipoDoFilme": "comédia musical romântica",
+    "diretor": "Carlos Alberto Ricceli",
+    "categoria": ["Comédia"],
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Rodrigo Lombardi",
+        "atrizPrincipal": "Bruna Lombardi",
+        "atorCoadjuvante":"Tiago Abravanel"
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"
+
+},
+{
+    "id": 4,
+    "titulo": "É fada",
+    "distribuidor": "Daniel Filho",
+    "tipoDoFilme": "comédia infanto juvenil",
+    "diretor": "Cris D'Amato",
+    "categoria": ["comédia","infanto juvenil","fantasia"],
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Silvio Guindane",
+        "atrizPrincipal": "Kéfera Buchmann",
+        "atorCoadjuvante":"Aramis Trindade "
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"
+   
+},
+{
+    "id": 5,
+    "titulo": "Fala comigo",
+    "distribuidor": "Vitrine Filmes",
+    "tipoDoFilme": "Drama",
+    "diretor": "	Felipe Sholl",
+    "categoria": "drama",
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Tom Karabachian",
+        "atrizPrincipal": "Karine Teles",
+        "atorCoadjuvante":""
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"
+      
+},
+{
+
+    "id": 6,
+    "titulo": "Os Dez Mandamentos ",
+    "distribuidor": ["Paris Filmes","Downtown Filmes"],
+    "tipoDoFilme": "Drama",
+    "diretor": "	Felipe Sholl",
+    "categoria": "	épico",
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Guilherme Winter",
+        "atrizPrincipal": "Giselle Itié",
+        "atorCoadjuvante":"Petrônio Gontijo"
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"    
+},
+{
+
+    "id": 7,
+    "titulo": "Entre Idas e Vindas ",
+    "distribuidor": "Imagem Filmes",
+    "tipoDoFilme": "comédia romântica",
+    "diretor": "José Eduardo Belmonte",
+    "categoria": "comédia",
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Fábio Assunçaõ",
+        "atrizPrincipal": "Ingrid Guimarães",
+        "atorCoadjuvante":"João Assunção"
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"    
+}, 
+{
+    
+    "id": 8,
+    "titulo": "O Shaolin do Sertão  ",
+    "distribuidor": "Paris Filmes",
+    "tipoDoFilme": "comédia",
+    "diretor": "Halder Gomes",
+    "categoria": "comédia",
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Edmilson Filho",
+        "atrizPrincipal": "Bruna Hamú",
+        "atorCoadjuvante":"Dedé Santanao"
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"      
+
+},
+{
+
+    "id": 9,
+    "titulo": "Mãe Só Há Uma",
+    "distribuidor": "Vitrine Filmes",
+    "tipoDoFilme": "drama",
+    "diretor": "Anna Muylaert",
+    "categoria": "drama",
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Daniel Botelho",
+        "atrizPrincipal": "Dani Nefussi ",
+        "atorCoadjuvante":"Matheus Nachtergaele"
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"  
+
+},
+{
+
+    "id": 10,
+    "titulo": "Tô Ryca",
+    "distribuidor": ["Paris Filmes", "Downtown Filmes"],
+    "tipoDoFilme": "comédia",
+    "diretor": "Pedro Antonio",
+    "categoria": "drama",
+    "anoLancamento": 2016,
+    "casting": {
+        "atorPrincipal": "Marcus Majella",
+        "atrizPrincipal": "Samantha Schmütz",
+        "atorCoadjuvante":"Marcelo Adnet"
+    },
+    "cor": "colorido",
+    "nacionalidade": "Brasil"  
+
+}
+]
     
     console.log(filmes)
     
